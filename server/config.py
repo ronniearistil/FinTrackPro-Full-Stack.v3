8f23aa264b12d623be6f7b89ea780cb034181244
Ronnie Aristil <ronnie.aristil@gmail.com>
2025-01-06 11:53:53 -0800

    Update to Relative Pay

--- server/config.py
+++ server/config.py
@@ -1,3 +1,41 @@
+# import os
+# 
+# class Config:
+#     SECRET_KEY = os.getenv("SECRET_KEY", "defaultsecretkey")
+#     SQLALCHEMY_DATABASE_URI = os.getenv(
+#         "DATABASE_URL",
+#         f"sqlite:///{os.path.join(os.path.abspath(os.getcwd()), 'instance', 'app.db')}"
+#     )
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "*")
+#     DEBUG = False
+# 
+#     # Email configuration
+#     MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+#     MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+#     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+#     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+#     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() in ["true", "1", "t"]
+#     MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False").lower() in ["true", "1", "t"]
+# 
+# 
+# class DevelopmentConfig(Config):
+#     DEBUG = True
+# 
+# 
+# class ProductionConfig(Config):
+#     DEBUG = False
+# 
+# 
+# config = {
+#     "default": Config,
+#     "development": DevelopmentConfig,
+#     "production": ProductionConfig,
+# }
+
+
+# Deployment Test
+
 import os
 
 class Config:
