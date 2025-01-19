8e06614e25f52c255ca773d9963e26baf8ed182e
Ronnie Aristil <ronnie.aristil@gmail.com>
2025-01-18 20:56:57 -0800

    Bundle frontend with backend for deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,8 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5555';
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5555';
+// export default API_URL;
+
+
+// Test 
+
+const API_URL = process.env.REACT_APP_API_URL || 'https://your-backend-url.onrender.com';
 export default API_URL;
